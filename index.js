79398aea78ab3246977db6a0c0966d0eb5df1ac9
Joshua Kiley <joshua.kiley.dev@gmail.com>
2020-09-18 15:16:07 -0400

    add minimal styling and add game reset functionality

---
+++ index.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("ready");
+
+    const setNumbers = () => {
+        const nums = [];
+        const boxes = document.getElementsByClassName("box");
+
+        document.getElementById("score").innerHTML = 100;
+
+
+        for (let i = 1; i <= 8; i++) {
+            nums.push(i);
+            nums.push(i);
+        }
+
+        for (let i = 0; i < boxes.length; i++) {
+            const tempIndex = Math.floor(Math.random() * Math.floor(nums.length))
+            boxes[i].innerHTML = `${nums[tempIndex]}`
+            nums.splice(tempIndex, 1);
+        }
+
+        console.log(nums)
+
+    }
+
+    const layBoard = () => {
+        for (let i = 0; i < 16; i++) {
+            let box = document.createElement("div");
+            box.classList.add("box");
+            box.setAttribute("id", `${i}`);
+            document.getElementById("game-board").append(box);
+        }
+        setNumbers();
+    }
+
+    document.getElementById("restart").addEventListener("click", setNumbers);
+
+    layBoard();
+});
